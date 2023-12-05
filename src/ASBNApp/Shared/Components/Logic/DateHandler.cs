6d7247cad54db7fffe3f3cfb4bc7f0da06e94b1c
mr-woodapple <39863485+mr-woodapple@users.noreply.github.com>
2023-12-05 09:34:21 +0100

    Implemented #24, minor css tweaks

--- src/ASBNApp/Shared/Components/Logic/DateHandler.cs
+++ src/ASBNApp/Shared/Components/Logic/DateHandler.cs
@@ -50,7 +50,7 @@ public class DateHandler {
     /// <summary>
     /// Returns the Monday date for a given week & year
     /// </summary>
-    /// <returns>DateTime object for the first day in a week</returns>
+    /// <returns> DateTime object for the first day in a week </returns>
     public DateTime GetFirstDateOfWeek(int week, int year) {
         // Get first Monday of a year
         DateTime FirstDayOfAYear = new DateTime(year, 1, 1);
@@ -58,5 +58,19 @@ public class DateHandler {
 
         // Calculate how many days we're in from that first Monday by the number of weeks
         return FirstMondayOfAYear.AddDays((week - 1) * 7);
+    }
+
+    /// <summary>
+    /// Returns the Sunday date for a given week & year
+    /// </summary>
+    /// <returns> DateTime object for the first day in a week </returns>
+    public DateTime GetLastDateOfWeek(int week, int year) {
+        // Get first Monday of a year
+        DateTime FirstDayOfAYear = new DateTime(year, 1, 1);
+        DateTime FirstMondayOfAYear = new DateTime(year, 1, (8 - (int)FirstDayOfAYear.DayOfWeek) % 7 + 1);
+
+        // Calculate how many days we're in from that first Monday by the number of weeks, 
+        // add 6 days to get the last date in a week
+        return FirstMondayOfAYear.AddDays((week - 1) * 7 + 6);
     }
 }
--- src/ASBNApp/Shared/Components/Logic/WorkLocationHours.cs
+++ src/ASBNApp/Shared/Components/Logic/WorkLocationHours.cs
@@ -5,7 +5,7 @@ public class WorkLocationHours {
     // demo data for testing purposes, to be removed later on! 
     public static List<WorkLocationHours> DemoWorkLocationData = new() { 
         new WorkLocationHours {Location = "MMS", Hours = 7.6},
-        new WorkLocationHours {Location = "HUB Dresden", Hours = 7.2},
-        new WorkLocationHours {Location = "BSZ ET DD", Hours = 7.6},
+        new WorkLocationHours {Location = "HUB Dresden", Hours = 6.2},
+        new WorkLocationHours {Location = "BSZ ET DD", Hours = 7.2},
     };
 }
