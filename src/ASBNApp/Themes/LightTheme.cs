5c585b1a9bab0b11a848aba5e17967e56de815b1
mr-woodapple <39863485+mr-woodapple@users.noreply.github.com>
2024-05-13 21:13:59 +0200

    Started working on the week view

--- src/ASBNApp/Themes/LightTheme.cs
+++ src/ASBNApp/Themes/LightTheme.cs
@@ -7,7 +7,9 @@ public static class LightTheme
         return new MudTheme
         {
             Palette = new PaletteLight
-            {     
+            {
+                Primary = Colors.Orange.Accent4,
+                AppbarBackground = Colors.Orange.Accent4,
                 AppbarText = Colors.Shades.White
             }
         };
