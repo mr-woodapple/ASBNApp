d5b9f4e5e1c4fc23ea72c515bc72bbedafba965b
mr-woodapple <jasper.holzapfel@mailbox.tu-dresden.de>
2024-07-20 00:52:56 +0200

    Added the log in UI, started working on an auth service

---
+++ src/ASBNApp.Frontend/Model/LoginWrapper.cs
@@ -0,0 +1,11 @@
+﻿namespace ASBNApp.Frontend.Model
+{
+	/// <summary>
+	/// Simple model used to serialize the login data correctly.
+	/// </summary>
+	public class LoginWrapper
+	{
+		public string EMail { get; set; }
+		public string Password { get; set; }
+	}
+}
--- src/ASBNApp.Frontend/Program.cs
+++ src/ASBNApp.Frontend/Program.cs
@@ -13,11 +13,10 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 // MudBlazor
 builder.Services.AddMudServices();
 
-// ASBNDataService dependency injection
+// Adding dependcy injection services
 builder.Services.AddScoped<IASBNDataService, ASBNDataService>();
-
-// DateHandler service
 builder.Services.AddSingleton<DateHandler>();
+builder.Services.AddSingleton<AuthHandler>();
 
 // Making the FileSystemAccess package available to everyone
 builder.Services.AddFileSystemAccessService();
@@ -29,6 +28,8 @@ builder.Services.AddScoped(sp => new HttpClient
 });
 // Add the font service used when generating .pdf's
 builder.Services.AddHttpClient<FontServices>(client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
+// AuthHandler http client
+builder.Services.AddHttpClient<AuthHandler>(client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
 // API httpClient
 builder.Services.AddHttpClient<IASBNDataService, ASBNDataService>(client => client.BaseAddress = new Uri("https://localhost:7148"));
 
