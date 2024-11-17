ff892d657c2be1f50fbf6f2aa21d5e592a81f85b
mr-woodapple <jasper.holzapfel@mailbox.tu-dresden.de>
2024-11-17 22:52:02 +0100

    Started working on the feature

---
+++ src/ASBNApp.Frontend/Enums/DataOperations.cs
@@ -0,0 +1,8 @@
+﻿namespace ASBNApp.Frontend.Enums
+{
+	public static class DataOperations
+	{
+		public static readonly string Import = "Import";
+		public static readonly string Export = "Export";
+	}
+}
