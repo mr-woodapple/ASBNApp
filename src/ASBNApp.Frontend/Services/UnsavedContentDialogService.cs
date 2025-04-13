d7c31d96a8add11b720e79947797fb34c7c03220
mr-woodapple <jasper.holzapfel@mailbox.tu-dresden.de>
2025-04-14 00:31:43 +0200

    Implemented confirmation dialog if the user tries to switch views, but has unsaved data

---
+++ src/ASBNApp.Frontend/Services/UnsavedContentDialogService.cs
@@ -0,0 +1,13 @@
+﻿namespace ASBNApp.Frontend.Services;
+
+// TODO: Make this a DI service, so it's not required to be static
+public static class UnsavedContentDialogService
+{
+	public static bool IsDayViewDataSaved { get; set; } = true;
+	public static bool IsWeekViewDataSaved { get; set; } = true;
+
+	public static void ResetDayView() => IsDayViewDataSaved = true;
+	public static void ResetWeekView() => IsWeekViewDataSaved = true;
+
+
+}
